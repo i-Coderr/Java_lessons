9782ff4b702a47b2844e9eb2dab021114a73f696
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-10 16:21:37 +0600

    if else statement

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,6 +1,36 @@
 public class Lesson4 {
     public static void main(String[] args) {
 
+/*        //TODO if statement
+        int x = 20;
+        int y = 18;
+
+        if (x > y) {
+            System.out.println("20 is greater than 18");
+        }*/
+
+
+
+/*        //TODO else statement
+        int time = 15;
+
+        if (time < 18) {
+            System.out.println("Good day!");
+        } else {
+            System.out.println("Good evening!");
+        }*/
+
+
+/*        //TODO else if statement
+        int time = 20;
+
+        if (time < 10){
+            System.out.println("Good morning!");
+        } else if (time < 18) {
+            System.out.println("Good day!");
+        } else {
+            System.out.println("Good evening!");
+        }*/
 
     }
 }
