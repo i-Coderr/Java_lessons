c9831dcee3cda3e948c13e267ceeb8b814409b9f
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-12 15:05:20 +0600

    add while v3

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -2,11 +2,10 @@ public class Lesson5 {
     public static void main(String[] args) {
 
 /*        //TODO while
-        int counter = 0;
-
-        while (counter < 10){
-            System.out.println("Hello");
-            counter++;
+        int counter = 0;                   // cчетчик
+        while (counter < 10){              // счетчик 10дон кичине болсо
+            System.out.println("Hello");   // консольго Hello деп чыгар
+            counter++;                     // счетчик 1ге көбөй
         }*/
 
 
