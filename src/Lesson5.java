3fbc71e7dd7d5c496b0169f134bf0f2b5d7bac69
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-12 14:57:25 +0600

    add while

---
+++ src/Lesson5.java
@@ -0,0 +1,14 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+
+/*        //TODO while
+        int a = 0;
+
+        while (a < 100){
+            System.out.println("Hello");
+            a++;
+        }*/
+
+
+    }
+}
