bf2865f050056058a5f75eed82125bd5c5f74095
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-13 13:42:29 +0600

    add homework#1

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Lesson5 {
     public static void main(String[] args) {
 
 /*        //TODO while
-        int counter = 0;                   // cчетчик
-        while (counter < 10){              // счетчик 10дон кичине болсо
-            System.out.println("Hello");   // консольго Hello деп чыгар
-            counter++;                     // счетчик 1ге көбөй
+        int counter = 0;                             // cчетчик
+        while (counter < 10){                        // счетчик 10дон кичине болсо
+            System.out.println(counter + "Hello");   // консольго Hello деп чыгар
+            counter++;                               // счетчик 1ге көбөй
         }*/
+
+
+/*        //TODO Сабактагы тапшырма#1
+        Scanner scanner = new Scanner(System.in);
+        int counter =scanner.nextInt();
+
+        while (counter>1){
+            counter --;
+            System.out.println(counter);
+        }*/
+
 
 
     }
