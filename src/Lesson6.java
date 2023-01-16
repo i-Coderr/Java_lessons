cdd77c0bd4ecfb6d1e8299bfdfe86a4e1b736e81
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-16 13:09:41 +0600

    add for loop sample

--- src/Lesson6.java
+++ src/Lesson6.java
@@ -1,14 +1,22 @@
 public class Lesson6 {
     public static void main(String[] args) {
 
+/*        //TODO for loop
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hello");
+        }*/
+
+
 /*        //TODO сабактагы тапшырма#1
         for (int i = 0; i<=10; i = i + 2){
             System.out.println(i);
         }*/
 
-        //TODO Infinite loop
+
+/*        //TODO Infinite loop
         for (;;) {
             System.out.println("Infinite Loop");
-        }
+        }*/
+
     }
 }
