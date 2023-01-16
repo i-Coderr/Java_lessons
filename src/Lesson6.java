87b5b26db18b117212cee9cb8f9bf000274c4ac0
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-16 12:40:00 +0600

    add infinite loop

--- src/Lesson6.java
+++ src/Lesson6.java
@@ -1,9 +1,14 @@
 public class Lesson6 {
     public static void main(String[] args) {
 
-        //TODO сабактагы тапшырма#1
+/*        //TODO сабактагы тапшырма#1
         for (int i = 0; i<=10; i = i + 2){
             System.out.println(i);
+        }*/
+
+        //TODO Infinite loop
+        for (;;) {
+            System.out.println("Infinite Loop");
         }
     }
 }
