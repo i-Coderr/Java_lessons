c2dd6f95a78e1038a409cae4e76594ecb1b87b0a
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-16 13:17:25 +0600

    add while loop sample

--- src/Lesson6.java
+++ src/Lesson6.java
@@ -1,17 +1,22 @@
 public class Lesson6 {
     public static void main(String[] args) {
 
+/*        //TODO while loop
+        int a = 0;
+        while (a < 10) {
+            System.out.println("Hello");
+            a++;
+        }*/
+
 /*        //TODO for loop
         for (int i = 0; i < 10; i++){
             System.out.println("Hello");
         }*/
 
-
 /*        //TODO сабактагы тапшырма#1
         for (int i = 0; i<=10; i = i + 2){
             System.out.println(i);
         }*/
-
 
 /*        //TODO Infinite loop
         for (;;) {
