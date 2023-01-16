6c3690f1fac01ab1a370f87640f756d7d73889b1
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-16 11:53:06 +0600

    add Lesson6

---
+++ src/Lesson6.java
@@ -0,0 +1,9 @@
+public class Lesson6 {
+    public static void main(String[] args) {
+
+        //TODO сабактагы тапшырма#1
+        for (int i = 0; i<=10; i = i + 2){
+            System.out.println(i);
+        }
+    }
+}
