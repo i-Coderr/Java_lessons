94259164a05be7e3e6887f6e76dca57de15fec9d
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-18 20:44:03 +0600

    add task#2

--- src/Lesson7.java
+++ src/Lesson7.java
@@ -1,20 +1,26 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lesson7 {
     public static void main(String[] args) {
 
 /*        //TODO String
         String greeting = "Hello";
-        String world = " World";
-        System.out.println(greeting);
+        String world = " World ";
         System.out.println(greeting.concat(world));
 
-        String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String text = "Hello";
         System.out.println("Текстин узундугу " + text.length());
 
         String txt = "Hello World!";
         System.out.println(txt.toUpperCase());
-        System.out.println(txt.toLowerCase());*/
+        System.out.println(txt.toLowerCase());
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = in.nextLine();
+        System.out.println(name);*/
 
 /*        //TODO array
         String car1 = "Porsche";
@@ -22,15 +28,46 @@ public class Lesson7 {
         String car3 = "Ford";
         String car4 = "Toyota";
 
-        String [] cars = {"Porsche", "BMW", "Ford", "Toyota"};
-        cars[0] = "Lamborghini";
-        System.out.println(cars[0]);
-        System.out.println(cars.length);
+        String [] garage = {"Porsche", "BMW", "Ford", "Toyota", "Lada", "Tesla"};
 
-                       // 0123456
-        String Porsche = "Porsche";
-        System.out.println(Porsche.indexOf("e"));*/
+        garage[2] = "Lamborghini";
+        System.out.println(garage[2]);
+        System.out.println();
 
+        for (int i = 0; i < garage.length; i++){
+            System.out.println(garage[i]);
+        }
+
+        System.out.println(garage.length);
+
+                       // 0123456789
+        String Porsche = "Porsche is my favorite car";
+        System.out.println(Porsche.indexOf("r"));*/
+
+/*        //TODO сабактагы тапшырма#1
+        int size = 10;
+        String [] numbers = new String[size];
+        numbers[0] = "1. Black";
+        numbers[1] = "2. White";
+        numbers[2] = "3. Red";
+        numbers[3] = "4. Green";
+        numbers[4] = "5. Yellow";
+        numbers[5] = "6. Pink";
+        numbers[6] = "7. Purple";
+        numbers[7] = "8. Blue";
+        numbers[8] = "9. Brown";
+        numbers[9] = "10. Orange";
+
+
+        //for each
+        for (String x : numbers) {
+            System.out.println(x);
+        }
+
+        // for loop
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }*/
 
 /*        //TODO for loop
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
@@ -44,7 +81,6 @@ public class Lesson7 {
             System.out.println(i);
         }*/
 
-
 /*        //TODO Random
         Random random = new Random();
         int size = 5;
@@ -57,6 +93,21 @@ public class Lesson7 {
         for (int a :array){
             System.out.println(a);
         }*/
+
+        //TODO сабактагы тапшырма#2
+        Random random = new Random();
+        int[] array = new int[10];
+
+        int average;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1, 20);
+            System.out.print(array[i] + " ");
+            sum += array[i];
+        }
+        average = sum / array.length;
+        System.out.println();
+        System.out.println("Average of random numbers: " + average);
 
     }
 }
