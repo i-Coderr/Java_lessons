571de33e5503d092ee1af1ed78179167e18d85d1
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-18 17:44:34 +0600

    add Lesson7

---
+++ src/Lesson7.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+
+/*        //TODO String
+        String greeting = "Hello";
+        String world = " World";
+        System.out.println(greeting);
+        System.out.println(greeting.concat(world));
+
+        String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("Текстин узундугу " + text.length());
+
+        String txt = "Hello World!";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());*/
+
+/*        //TODO array
+        String car1 = "Porsche";
+        String car2 = "BMW";
+        String car3 = "Ford";
+        String car4 = "Toyota";
+
+        String [] cars = {"Porsche", "BMW", "Ford", "Toyota"};
+        cars[0] = "Lamborghini";
+        System.out.println(cars[0]);
+        System.out.println(cars.length);
+
+                       // 0123456
+        String Porsche = "Porsche";
+        System.out.println(Porsche.indexOf("e"));*/
+
+
+/*        //TODO for loop
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }*/
+
+/*        //TODO for each
+        String[] cars2 = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars2) {
+            System.out.println(i);
+        }*/
+
+
+/*        //TODO Random
+        Random random = new Random();
+        int size = 5;
+        int[] array = new int[size];;
+
+        for (int i = 0; i < size; i++){
+            int randomNumbers = random.nextInt(100);
+            array[i] = randomNumbers;
+        }
+        for (int a :array){
+            System.out.println(a);
+        }*/
+
+    }
+}
