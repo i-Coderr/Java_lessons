8ed29d21fa0c31871515160561ead164dc273205
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-20 15:04:16 +0600

    add multiple parameters method

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -2,10 +2,24 @@ public class Lesson8 {
     public static void main(String[] args) {
 
         myMethod();
+        methodWithParameter("Java");
+        methodWithMultipleParameter("Chyngyz", 36);
     }
 
+    //TODO эч нерсе кайтарбаган метод
     static void myMethod(){
-        System.out.println("Hello function");
+        System.out.println("This is void method");
+    }
+
+
+    //TODO параметри менен метод
+    static void methodWithParameter(String name){
+        System.out.println("Hello " + name);
+    }
+
+    //TODO көп параметрлүү метод
+    static void methodWithMultipleParameter(String name, int age){
+        System.out.println(name + " is " + age);
     }
 
 
