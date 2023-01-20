1e4d5147e56dbe0d2eb2520dacef0c0dbfb72dbe
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-20 17:58:52 +0600

    add final

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -1,27 +1,49 @@
 public class Lesson8 {
     public static void main(String[] args) {
 
-        myMethod();
-        methodWithParameter("Java");
-        methodWithMultipleParameter("Chyngyz", 36);
     }
 
     //TODO эч нерсе кайтарбаган метод
-    static void myMethod(){
+    static void myMethod() {
         System.out.println("This is void method");
     }
 
 
     //TODO параметри менен метод
-    static void methodWithParameter(String name){
+    static void methodWithParameter(String name) {
         System.out.println("Hello " + name);
     }
 
     //TODO көп параметрлүү метод
-    static void methodWithMultipleParameter(String name, int age){
+    static void methodWithMultipleParameter(String name, int age) {
         System.out.println(name + " is " + age);
     }
 
 
+ /*   //TODO эки сандын суммасын тапкан метод
+    static void sumOfTwoNumbers(int a, int b) {
+        System.out.println(a + b);
+    }*/
+
+/*    //TODO так же жуп санды тапкан метод
+    static void findEvenOrOddNumbers(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " бул жуп сан");
+        } else {
+            System.out.println(num + " бул так сан");
+        }
+    }*/
+
+/*    //TODO минимальный санды тапкан метод
+    static void findMinNumber(int a, int b){
+        System.out.println(Math.min(a, b));
+    }*/
+
+/*    //TODO массивди колсольго чыгарат
+    static void getArray(int [] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }*/
 
 }
