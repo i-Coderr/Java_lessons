6e4de6696826df5da82ffd600cc64d5f7eb79ba6
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-20 14:29:06 +0600

    add Lesson8

---
+++ src/Lesson8.java
@@ -0,0 +1,13 @@
+public class Lesson8 {
+    public static void main(String[] args) {
+
+        myMethod();
+    }
+
+    static void myMethod(){
+        System.out.println("Hello function");
+    }
+
+
+
+}
