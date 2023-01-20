f5ca8cf5c176d592e2414c6da10279438525d00b
Chyngyz Sharshekeev <76399128+i-Coderr@users.noreply.github.com>
2023-01-20 20:36:35 +0600

    add finalizing

--- src/Lesson8.java
+++ src/Lesson8.java
@@ -1,28 +1,42 @@
+import java.util.Arrays;
+
 public class Lesson8 {
     public static void main(String[] args) {
 
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] numbers2 = {100, 101, 102, 103, 104, 105};
+        array(numbers);
+        array(numbers2);
+
+        }
+
+    //TODO Массивдин ичиндеги элементтерди чыгарып бере турган метод
+    static void array(int[] numbers) {
+        for (int print : numbers){
+            System.out.println(print);
+        }
     }
 
-    //TODO эч нерсе кайтарбаган метод
-    static void myMethod() {
-        System.out.println("This is void method");
-    }
-
-
-    //TODO параметри менен метод
+/*    //TODO параметри менен метод
     static void methodWithParameter(String name) {
         System.out.println("Hello " + name);
-    }
+    }*/
 
-    //TODO көп параметрлүү метод
+/*
+    //TODO эч нерсе кайтарбаган метод
+    static void myFirstVoidMethod() {
+        System.out.println("This is void method");
+    }
+*/
+
+/*    //TODO көп параметрлүү метод
     static void methodWithMultipleParameter(String name, int age) {
         System.out.println(name + " is " + age);
-    }
+    }*/
 
-
- /*   //TODO эки сандын суммасын тапкан метод
+/*    //TODO эки сандын суммасын тапкан метод
     static void sumOfTwoNumbers(int a, int b) {
-        System.out.println(a + b);
+        System.out.println("Эки сандын суммасы: " + (a + b));
     }*/
 
 /*    //TODO так же жуп санды тапкан метод
@@ -39,11 +53,5 @@ public class Lesson8 {
         System.out.println(Math.min(a, b));
     }*/
 
-/*    //TODO массивди колсольго чыгарат
-    static void getArray(int [] array){
-        for (int i = 0; i < array.length; i++){
-            System.out.println(array[i]);
-        }
-    }*/
-
 }
+
